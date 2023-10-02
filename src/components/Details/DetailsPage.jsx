a40097cdab739e725f4c267879141779109057cc
Getachew Hundera <ghundera1@gmail.com>
2023-10-02 04:27:33 -0500

    created a DetailsPage component, installed material UI, added a temp Button in MoveiList.jsx, and a button to get back to MovieList in Detailspage.jsx

---
+++ src/components/Details/DetailsPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './DetailsPage.css'
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+
+
+
+
+function DetailsPage() {
+
+    return(
+        <>
+        <h1> This is the Details Page </h1>
+
+
+        <Button
+                    component={Link}
+                    to={"/"}
+                    variant="contained"
+                    color="primary"
+                > Details page </Button>
+
+
+  
+
+
+
+        </> 
+
+
+    ); 
+}; 
+
+export default DetailsPage; 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+
 
 function MovieList() {
 
@@ -11,6 +14,8 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+
+
     return (
         <main>
             <h1>MovieList</h1>
@@ -19,11 +24,19 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} />
                         </div>
                     );
                 })}
             </section>
+{/* temporary button to get to the details page view  */}
+            <Button
+                    component={Link}
+                    to={"/DetailsPage"}
+                    variant="contained"
+                    color="primary"
+                > Details page </Button>
+
         </main>
 
     );
